cec5eab1347deb078754c126e57c941a23c1b0c6
MAlhussaini <MAlhussaini56@gmail.com>
2021-10-30 15:54:31 +0300

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// mainIcon = document.getElementById("mainIcon");
+// mainIcon.addEventListener("click", home_redirect);
+
+function home_redirect() {
+    console.log("Click");
+    location.replace("./home.html");
+}
